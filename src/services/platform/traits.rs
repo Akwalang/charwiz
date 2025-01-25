db988ac16adbb490dd375a458fe3d9290f597c0f
Siarhei Kukunin <siarhei.kukunin@btbisrael.co.il>
2025-01-25 14:26:33 +0100

    feat: update platform methods

--- src/services/platform/traits.rs
+++ src/services/platform/traits.rs
@@ -4,6 +4,6 @@ pub trait Platform {
   fn get_keyboard_layout_by_id(&self, id: &str) -> Option<&KeyboardLayout>;
   fn get_current_keyboard_layout(&self) -> &KeyboardLayout;
   fn get_next_keyboard_layout(&self) -> &KeyboardLayout;
-  fn set_keyboard_layouts(&mut self, layout_id: &str) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>>;
-  fn switch_keyboard_layout(&mut self) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>>;
+  fn set_keyboard_layouts(&self, layout_id: &str) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>>;
+  fn switch_keyboard_layout(&self) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>>;
 }
--- src/services/platform/windows/windows.rs
+++ src/services/platform/windows/windows.rs
@@ -3,19 +3,17 @@ use super::utils;
 use crate::services::{KeyboardLayout, PlatformTrait};
 
 pub struct Windows {
-  current_keyboard_layout_id: String,
   keyboard_layouts: Vec<KeyboardLayout>,
 }
 
 impl Windows {
   pub fn new() -> Windows {
-    let current_keyboard_layout_id = utils::get_current_keyboard_layout_id();
     let keyboard_layouts = utils::get_keyboard_layouts();
 
-    println!("Current keyboard layout: {}", current_keyboard_layout_id);
+    println!("Current keyboard layout: {}", utils::get_current_keyboard_layout_id());
     println!("Keyboard layouts: {:?}", keyboard_layouts);
 
-    Windows { current_keyboard_layout_id, keyboard_layouts }
+    Windows { keyboard_layouts }
   }
 }
 
@@ -27,11 +25,13 @@ impl PlatformTrait for Windows {
   }
 
   fn get_current_keyboard_layout(&self) -> &KeyboardLayout {
-    &self.get_keyboard_layout_by_id(&self.current_keyboard_layout_id).unwrap()
+    let id = utils::get_current_keyboard_layout_id();
+
+    &self.get_keyboard_layout_by_id(&id).unwrap()
   }
 
   fn get_next_keyboard_layout(&self) -> &KeyboardLayout {
-    let id = &self.current_keyboard_layout_id;
+    let id = utils::get_current_keyboard_layout_id();
     let lts = &self.keyboard_layouts;
 
     let idx: usize = lts.iter().position(|lt| lt.id == *id).unwrap();
@@ -40,7 +40,7 @@ impl PlatformTrait for Windows {
     &lts.get(next_idx).unwrap()
   }
 
-  fn set_keyboard_layouts(&mut self, layout_id: &str) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>> {
+  fn set_keyboard_layouts(&self, layout_id: &str) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>> {
     let lt = self.get_keyboard_layout_by_id(layout_id);
 
     if lt.is_none() { return Ok(None); }
@@ -49,14 +49,12 @@ impl PlatformTrait for Windows {
 
     utils::switch_global_keyboard_layout(layout_id)?;
 
-    self.current_keyboard_layout_id = layout_id.to_string();
-
     println!("Switched to keyboard layout: {} ({})", lt.name, layout_id);
 
     Ok(Some(lt))
   }
 
-  fn switch_keyboard_layout(&mut self) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>> {
+  fn switch_keyboard_layout(&self) -> Result<Option<KeyboardLayout>, Box<dyn std::error::Error + 'static>> {
     let next_id = self.get_next_keyboard_layout().id.clone();
 
     self.set_keyboard_layouts(&next_id)
