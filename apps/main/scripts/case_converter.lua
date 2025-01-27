57845bd81ab42c6c7e800fa7da2aaeed7bb2fa55
Siarhei Kukunin <kukunins@gmail.com>
2025-01-27 22:00:42 +0100

    feat: add case convert plugins

---
+++ apps/main/scripts/case_converter.lua
@@ -0,0 +1,45 @@
+local function split_into_words(input)
+    local normalized = input:gsub("[-_ ]+", " ")
+
+    local words = {}
+
+    for word in normalized:gmatch("%S+") do
+        table.insert(words, word)
+    end
+
+    return words
+end
+
+function to_snake_case(input)
+    local words = split_into_words(input)
+
+    return table.concat(words, "_"):lower()
+end
+
+function to_pascal_case(input)
+    for i, word in ipairs(words) do
+        words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
+    end
+
+    return table.concat(words)
+end
+
+function to_camel_case(input)
+    local words = split_into_words(input)
+
+    for i, word in ipairs(words) do
+        if i > 1 then
+            words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
+        else
+            words[i] = word:lower()
+        end
+    end
+
+    return table.concat(words)
+end
+
+function to_kebab_case(input)
+    local words = split_into_words(input)
+
+    return table.concat(words, "-"):lower()
+end
