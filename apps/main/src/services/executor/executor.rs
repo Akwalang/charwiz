d00dcf761036a92d6d26d8bc9902b70f31a64502
Siarhei Kukunin <kukunins@gmail.com>
2025-01-30 14:37:38 +0100

    chore: refactor apply function

--- apps/main/src/services/executor/executor.rs
+++ apps/main/src/services/executor/executor.rs
@@ -38,28 +38,16 @@ impl Executor {
   pub async fn apply(&mut self, action: &ActionConfig) -> Result<(), Box<dyn std::error::Error>> {
     self.clipboard.save();
 
-    let kbl_before = self.platform.get_current_keyboard_layout();
-
-    if action.switch_keyboard_layout { self.platform.switch_keyboard_layout()?; }
-
-    let kbl_after = self.platform.get_current_keyboard_layout();
-
-    Self::prepare_selection(action).await?;
-
-    Self::copy_to_clipboard(action).await?;
-
-    let value = self.transform_clipboard(action, kbl_before, kbl_after)?;
-
-    keyboard::utils::paste().await?;
-
-    if action.keep_selection { keyboard::utils::select_chars(value.chars().count()).await?; }
+    self.prepare_selection(action).await?;
+    self.copy_to_clipboard(action).await?;
+    self.use_transformation(action).await?;
 
     self.clipboard.restore();
 
     Ok(())
   }
 
-  async fn prepare_selection(action: &ActionConfig) -> Result<(), Box<dyn std::error::Error>> {
+  async fn prepare_selection(&self, action: &ActionConfig) -> Result<(), Box<dyn std::error::Error>> {
     match action.target {
       ActionTarget::All => keyboard::utils::select_all().await?,
       ActionTarget::Line => keyboard::utils::select_line().await?,
@@ -70,13 +58,35 @@ impl Executor {
     Ok(())
   }
 
-  async fn copy_to_clipboard(action: &ActionConfig) -> Result<(), Box<dyn std::error::Error>> {
+  async fn copy_to_clipboard(&self, action: &ActionConfig) -> Result<(), Box<dyn std::error::Error>> {
     match action.target {
       ActionTarget::Clipboard => (),
       _ => keyboard::utils::copy().await?,
     }
 
     Ok(())
+  }
+
+  async fn use_transformation(&self, action: &ActionConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let (kbl_before, kbl_after) = self.switch_keyboard_layout().await?;
+
+    let value = self.transform_clipboard(action, kbl_before, kbl_after)?;
+
+    keyboard::utils::paste().await?;
+
+    if action.keep_selection { keyboard::utils::select_chars(value.chars().count()).await?; }
+
+    Ok(())
+  }
+
+  async fn switch_keyboard_layout(&self) -> Result<(&KeyboardLayout, &KeyboardLayout), Box<dyn std::error::Error>> {
+    let before = self.platform.get_current_keyboard_layout();
+
+    self.platform.switch_keyboard_layout()?;
+
+    let after = self.platform.get_current_keyboard_layout();
+
+    Ok((before, after))
   }
 
   fn transform_clipboard(
