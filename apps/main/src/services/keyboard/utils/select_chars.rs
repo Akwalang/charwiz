69d91436acfd7b8e6ac760f703052dfcffd9800c
Siarhei Kukunin <kukunins@gmail.com>
2025-01-30 14:23:21 +0100

    feat: add limit on char selection count

--- apps/main/src/services/keyboard/utils/select_chars.rs
+++ apps/main/src/services/keyboard/utils/select_chars.rs
@@ -3,9 +3,15 @@ use super::{trigger_event, trigger_event_ms};
 use rdev::Key;
 
 pub async fn select_chars(count: usize) -> Result<(), Box<dyn std::error::Error>> {
+  select_chars_limited(count, 100usize).await
+}
+
+pub async fn select_chars_limited(count: usize, limit: usize) -> Result<(), Box<dyn std::error::Error>> {
   // Trigger select and move to left char [Shift + LeftArrow]
 
   if count == 0 { return Ok(()); }
+
+  let count = count.min(limit);
 
   trigger_event(Key::ShiftLeft, true).await?;
 
