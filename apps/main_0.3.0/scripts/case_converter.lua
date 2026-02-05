eb9b5d9cecfc7b3a83f91e6b93e4e26c4bb8a12f
Siarhei Kukunin <kukunins@gmail.com>
2026-02-05 01:04:46 +0100

    feat: update settings

--- apps/main_0.3.0/scripts/case_converter.lua
+++ apps/main_0.3.0/scripts/case_converter.lua
@@ -6,7 +6,9 @@ local function split_into_words(input)
     local str = input
 
     str = str:gsub("[-_ ]+", " ")
-    str = str:gsub("([A-Z])", " %1")
+    str = str:gsub("(%l)(%u)", "%1 %2")
+    str = str:gsub("(%u+)(%u%l)", "%1 %2")
+
     str = trim(str)
 
     local words = {}
@@ -19,43 +21,90 @@ local function split_into_words(input)
 end
 
 function to_text_case(input)
-    local words = split_into_words(input.value)
+    local result = {}
 
-    return table.concat(words, " "):lower()
-end
-
-function to_snake_case(input)
-    local words = split_into_words(input.value)
-
-    return table.concat(words, "_"):lower()
-end
-
-function to_pascal_case(input)
-    local words = split_into_words(input.value)
-
-    for i, word in ipairs(words) do
-        words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
-    end
-
-    return table.concat(words)
-end
-
-function to_camel_case(input)
-    local words = split_into_words(input.value)
-
-    for i, word in ipairs(words) do
-        if i > 1 then
-            words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
+    for line in input.value:gmatch("([^\n]*)\n?") do
+        if line == "" then
+            table.insert(result, "")
         else
-            words[i] = word:lower()
+            local words = split_into_words(line)
+            table.insert(result, table.concat(words, " "):lower())
         end
     end
 
-    return table.concat(words)
+    return table.concat(result, "\n")
+end
+
+function to_snake_case(input)
+    local result = {}
+
+    for line in input.value:gmatch("([^\n]*)\n?") do
+        if line == "" then
+            table.insert(result, "")
+        else
+            local words = split_into_words(line)
+            table.insert(result, table.concat(words, "_"):lower())
+        end
+    end
+
+    return table.concat(result, "\n")
+end
+
+function to_pascal_case(input)
+    local result = {}
+
+    for line in input.value:gmatch("([^\n]*)\n?") do
+        if line == "" then
+            table.insert(result, "")
+        else
+            local words = split_into_words(line)
+
+            for i, word in ipairs(words) do
+                words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
+            end
+
+            table.insert(result, table.concat(words, ""))
+        end
+    end
+
+    return table.concat(result, "\n")
+end
+
+function to_camel_case(input)
+    local result = {}
+
+    for line in input.value:gmatch("([^\n]*)\n?") do
+        if line == "" then
+            table.insert(result, "")
+        else
+            local words = split_into_words(line)
+
+            for i, word in ipairs(words) do
+                if i > 1 then
+                    words[i] = word:sub(1, 1):upper() .. word:sub(2):lower()
+                else
+                    words[i] = word:lower()
+                end
+            end
+
+            table.insert(result, table.concat(words, ""))
+        end
+    end
+
+    return table.concat(result, "\n")
 end
 
 function to_kebab_case(input)
-    local words = split_into_words(input.value)
+    local result = {}
 
-    return table.concat(words, "-"):lower()
+    for line in input.value:gmatch("([^\n]*)\n?") do
+        if line == "" then
+            table.insert(result, "")
+        else
+            local words = split_into_words(line)
+            table.insert(result, table.concat(words, "-"):lower())
+        end
+    end
+
+    return table.concat(result, "\n")
 end
